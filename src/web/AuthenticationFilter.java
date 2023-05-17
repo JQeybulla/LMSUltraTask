e7a97d3ed876d176d4adf8a17ed78151f7ad1a54
JQeybulla <qeybulla38@mail.ru>
2023-05-17 10:00:51 +0400

    configured AuthenticationFilter

--- src/web/AuthenticationFilter.java
+++ src/web/AuthenticationFilter.java
@@ -4,8 +4,10 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
 import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-
 
 public class AuthenticationFilter implements Filter {
 
@@ -19,8 +21,19 @@ public class AuthenticationFilter implements Filter {
     }
 
     @Override
-    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
 
+        // Check if the user is logged in
+        if (httpRequest.getSession().getAttribute("user") == null) {
+            // User is not logged in, redirect to the login page or show access denied message
+            httpRequest.getSession().setAttribute(Constant.MESSAGE, "Sistemə giriş edin və ya qeydiyyatdan keçin");
+            httpResponse.sendRedirect(httpRequest.getContextPath() + "/"+ Constant.PAGE_LOGIN);
+        } else {
+            // User is logged in, allow the request to proceed
+            filterChain.doFilter(request, response);
+        }
     }
 
     @Override
---
+++ src/web/LogoutServlet.java
@@ -0,0 +1,26 @@
+package web;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebServlet("/logout")
+public class LogoutServlet extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        HttpSession session = request.getSession(false); // Get the current session, but do not create a new one if it doesn't exist
+
+        if (session != null) {
+            session.invalidate(); // Invalidate the session, effectively logging out the user
+        }
+
+        // Redirect the user to a logged-out page or the login page
+        response.sendRedirect(request.getContextPath() + "/" + Constant.PAGE_LOGIN);
+    }
+}
